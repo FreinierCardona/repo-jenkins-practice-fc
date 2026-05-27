769e3d78309de4def3760736482cefaea8d74969
FreinierCardona <cardonafreinier@gmail.com>
2026-05-26 22:06:21 -0500

    Corregir el error

--- src/main/java/com/sena/practica_jenkins/PracticaJenkinsApplication.java
+++ src/main/java/com/sena/practica_jenkins/PracticaJenkinsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PracticaJenkinsApplication {
 
-	public static void main() {
+	public static void main(String[] args) {
 		SpringApplication.run(PracticaJenkinsApplication.class, args);
 	}
 
